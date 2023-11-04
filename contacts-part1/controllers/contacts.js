db6c35104ae9e94e4de6440d4de729af6b6eae16
almabenav2 <almabenavides02@gmail.com>
2023-11-03 23:45:58 -0500

    changes

---
+++ contacts-part1/controllers/contacts.js
@@ -0,0 +1,25 @@
+// const { ObjectId } = require('mongodb');
+const mongobd = require('../data/database');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res) => {
+    const result = await mongobd.getDatabase().db().collection('contacts').find();
+    result.toArray().then((contacts) => {
+        res.setHeader('Contect-Type', 'application/json');
+        res.status(200).json(contacts);
+    });
+};
+
+const getSingle = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongobd.getDatabase().db().collection('contacts').find({ _id: userId });
+    result.toArray().then((contacts) => {
+        res.setHeader('Contect-Type', 'application/json');
+        res.status(200).json(contacts[0]);
+    });
+};
+
+module.exports = {
+    getAll,
+    getSingle
+};
--- contacts-part1/routes/index.js
+++ contacts-part1/routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => { res.send('hello');});
+// router.get('/', (req, res) => { res.send('hello');});
+router.use('./contacts', require('./contacts'));
 
 module.exports = router;
